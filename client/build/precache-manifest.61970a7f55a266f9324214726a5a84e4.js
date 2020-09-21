1af059024535b17f934c04a2c6626da766ab174d
Hammadsoloman <hammadali95@yahoo.com>
2020-09-21 11:54:34 +0300

    add after refactore and solve DB problem

---
+++ client/build/precache-manifest.61970a7f55a266f9324214726a5a84e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b392847193ee8d0b4ae64f171626865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b86c56782d071de0a51",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b86c56782d071de0a51",
+    "url": "/static/js/main.4a801c9f.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
